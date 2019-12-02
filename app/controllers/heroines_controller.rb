6208ad313ba677d12492070f90dd3a2d9cc8e3d3
rachfuerst <rach.fuerst@gmail.com>
2019-12-02 11:34:27 -0600

    Finished Initial Code Challange, moving on to bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,34 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine= Heroine.find(params[:id])
+    # for some reason I could not get the following to pull the data in my show file: 
+      # @heroine.power.name
+    # To fix it, I'm trying to write some code to pull that data:
+    power_id = @heroine.power_id
+    @power= Power.find(power_id)
+    
+  end
+
+  def new
+    @heroine= Heroine.new
+  end
+
+  def create
+    @heroine= Heroine.new(heroine_params)
+
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
