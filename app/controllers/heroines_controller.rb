ac7626dfe11497c982dbd44f957bd6f654f2ae08
Buford Eeds <bufordeeds8@gmail.com>
2019-12-02 12:01:19 -0600

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,33 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(allowed_params)
+    if @heroine.valid?
+      x = allowed_params[:name].split(" ")
+      y = x.join
+      @heroine.save
+      redirect_to heroines_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+
+
+  private
+
+def allowed_params
+  params.require(:heroine).permit(:name, :super_name, :power_id)
+end
 end
