bc0c3a69b05df34722cac46462bfdebe31a0e3a2
fmwong77 <fongmeiw@gmail.com>
2019-12-02 11:59:14 -0600

    initial commit

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,16 @@
 class PowersController < ApplicationController
+  before_action :find_by_id, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+    
+  end
+
+  private 
+  def find_by_id
+    @power = Power.find(params[:id])
   end
 end
