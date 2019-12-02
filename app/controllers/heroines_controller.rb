726e2afc47cca7d55e906e877789584af8df6b18
github username <github email address>
2019-12-02 12:26:20 -0500

    gf-code-challenge-2

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,24 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create 
+    @heroine = Heroine.new(h_params)
+    @heroine.save
+    redirect_to @heroine
+  end  
+
+  private
+  
+  def h_params
+    params.require(:heroine).permit! 
+  end
 end
