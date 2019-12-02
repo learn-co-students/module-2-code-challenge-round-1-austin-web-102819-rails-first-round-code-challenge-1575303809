4e94a691b071e8325587fff6faddd1d51e2e515d
github username <github email address>
2019-12-02 12:00:33 -0600

    submission for deadline

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,19 @@
 class PowersController < ApplicationController
+  
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = find_power
+  end 
+
+
+
+
+  private
+
+  def find_power
+    Power.find(params[:id])
+  end 
 end
