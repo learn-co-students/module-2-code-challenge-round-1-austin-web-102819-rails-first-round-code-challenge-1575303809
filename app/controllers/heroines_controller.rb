3568f953e4c7f2ac7e34f97804dfc25cf959bd6b
masonwaller <masonwaller805@gmail.com>
2019-12-02 11:17:08 -0600

    finished all but bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,24 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end
+  def new
+    @heroine = Heroine.new
+  end 
+  def create
+    @heroine = Heroine.new 
+    if @heroine.valid?
+    @heroine.save(heroine_params)
+    redirect_to @heroine
+    else 
+      render new_heroine_path
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
