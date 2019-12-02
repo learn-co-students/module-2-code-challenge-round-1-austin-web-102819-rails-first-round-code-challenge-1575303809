d9cdb6b7a4efce840c1886c7d051c3835aaf8072
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-12-02 11:57:15 -0600

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+    
+  
+
+    # byebug
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params(:name,:super_name,:power_id))
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params(*args)
+    params.require(:heroine).permit(args)
+  end
 end
+
